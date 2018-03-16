9851abda0cdf7d2ffd6729474d452aad3eb96ef2
Humberto Vieira de Castro <humberto.castro@mira.vc>
2018-03-15 22:49:09 -0300

    init do projeto

---
+++ ProjetoCompiladores/ProjetoCompiladores/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProjetoCompiladores
+//
+//  Created by Humberto Vieira on 15/03/18.
+//  Copyright © 2018 GHP Enterprises. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProjetoCompiladores/ProjetoCompiladores/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProjetoCompiladores
+//
+//  Created by Humberto Vieira on 15/03/18.
+//  Copyright © 2018 GHP Enterprises. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
