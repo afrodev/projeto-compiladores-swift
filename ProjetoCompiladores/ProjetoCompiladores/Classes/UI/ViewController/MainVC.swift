8beb7e267c855ec3b8d83233af9dd271372ffca5
Humberto Vieira de Castro <humberto.castro@mira.vc>
2018-03-30 13:01:04 -0300

    Organização dos arquivos

---
+++ ProjetoCompiladores/ProjetoCompiladores/Classes/UI/ViewController/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  ProjetoCompiladores
+//
+//  Created by Humberto Vieira on 30/03/18.
+//  Copyright © 2018 GHP Enterprises. All rights reserved.
+//
+
+import Cocoa
+
+class MainVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        print("Entrou")
+    }
+    
+}
